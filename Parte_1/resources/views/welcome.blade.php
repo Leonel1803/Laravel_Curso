ccffa8442dd48ae94cb060ea710e5a1dd55278f8
Leon Rivera <leonrivera2003@gmail.com>
2023-10-12 22:59:39 -0600

    Inicio del Curso

---
+++ Parte_1/resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+           
+        </style>
+    </head>
+    <body class="antialiased">
+        <ul>
+            <li><a href="/Laravel_Curso/Parte_1/public/">Home</a></li>
+            <li><a href="/Laravel_Curso/Parte_1/public/contact">Contacto</a></li>
+            <li><a href="/Laravel_Curso/Parte_1/public/blog">Blog</a></li>
+            <li><a href="/Laravel_Curso/Parte_1/public/about">About</a></li>
+        </ul>
+        <h1>Welcome</h1>
+    </body>
+</html>
